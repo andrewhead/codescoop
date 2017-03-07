0818371a6079999a2b7781f9f84f601b7e5bcac3
Elena Glassman <elena.glassman@gmail.com>
2017-03-07 09:47:14 -0800

    added JAVA_HOME def

--- java/runtests.sh
+++ java/runtests.sh
@@ -3,6 +3,7 @@
 # Make list of jars that are needed for compiling and running tests
 JUNIT_JARS=./libs/junit-4.12.jar:./libs/hamcrest-core-1.3.jar
 SOOT_JARS=`ls -d -1 ./libs/*.jar | tr '\n' ':'`
+JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 JAVA_HOME_JARS=`ls -d -1 $JAVA_HOME/jre/lib/*.jar | tr '\n' ':'`
 JDI_JARS=$JAVA_HOME/lib/tools.jar
 
